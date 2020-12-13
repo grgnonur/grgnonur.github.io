497963e234656caeb73dd88a775b50f6413e1f0f
grgnonur <kskonurgurgen@hotmail.com>
2020-12-13 17:27:45 +0300

    website
    mywebsite

---
+++ script/script.js
@@ -0,0 +1,41 @@
+// $(document).ready(function(){
+//     // Add scrollspy to <body>
+//     $('body').scrollspy({target: ".navbar", offset: 50});   
+  
+//     // Add smooth scrolling on all links inside the navbar
+//     $("#navbarSupportedContent a").on('click', function(event) {
+  
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+  
+//       // Store hash
+//       var hash = this.hash;
+  
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
+     
+//         // Add hash (#) to URL when done scrolling (default click behavior)
+//         window.location.hash = hash;
+//       });
+//     });
+//   });
+  $(document).ready(function(){
+    $(".nav-link").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+  
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+  
